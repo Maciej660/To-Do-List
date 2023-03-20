8ca868881bd7f5e711d46f43bc7053db28716be4
Maciej.Konitz <maciej.konitz@wp.pl>
2023-03-20 17:41:09 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+{
+    const tasks = [
+        {
+
+        },
+        {
+
+        },
+    ];
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += 
+                <li>
+                    ${task.done ? "style\"text-decoration: line-trought\"" : ""}
+                </li>
+        }
+    }
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+    ;
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefult();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent
+            });
+            render()
+        });
+    };
+
+    init();
+
+}
