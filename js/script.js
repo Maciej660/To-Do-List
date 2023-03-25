35c00c0b96a8e52741f6889b69fbdfd232c636c8
Maciej.Konitz <maciej.konitz@wp.pl>
2023-03-25 16:56:57 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 {
     const tasks = [];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-            <li 
-            class="task__item">
-            <button class="form__buttonDone form__buttonDone--click js-done"> 
-            ${task.done ? "✔" : ""}
-            </button>
-            <span class="form__text ${task.done ? " task__decoration " : ""}">${task.content} </span>
-            <button class="js-remove form__buttonRemove">🗑</button>
+            <li> 
+            <button class="js-remove">usuń</button>
+            ${task.content}
                 </li>
                 `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-  
+
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -26,16 +34,12 @@
                 removeTask(index);
             });
         });
+
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-        render();
-    };
-    
-    const onFormSubmit =  (event) => {
+
+
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
@@ -58,4 +62,3 @@
     init();
 }
 
- 
