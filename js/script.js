59f4d55892981f9226d39cae435a40cf87bba58a
Maciej.Konitz <maciej.konitz@wp.pl>
2023-04-07 18:53:54 +0200

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 {
-    const tasks = [];
+    let tasks = [];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];        
         render();
     };
 
@@ -36,30 +37,39 @@
         });
     };
 
+const renderTasks = () => {       
+     let htmlString = "";
 
+for (const task of tasks) {
+    htmlString += `
+ <li class="task__item">
+    <button class="form__buttonDone form__buttonDone--click js-done">
+    ${task.done ? "✔" : ""}
+    </button>
+    <span class="form__text${ task.done ? "task__decoration" : ""}">
+    ${task.content} 
+    </span>
+    <button class="js-remove form__buttonRemove">
+    🗑
+    </button>
+ </li>
+        
+`;
+}
+
+document.querySelector(".js-tasks").innerHTML = htmlString;
+bindEvents();
+
+};
+
+const renderButtons = () => {}; /będzie renderować dwa przyciski, zróļ tutaj html-a tak jak w renderTasks/
+
+const bindButtonsEvents = () => {}; /dodaj addEventListener do przycisków ,tutaj będzie potrzebne jakiś if/
     const render = () => {
-        let htmlString = "";
+   renderTasks();
+   renderButtons();
+   bindButtonsEvents ();
 
-        for (const task of tasks) {
-            htmlString += `
-         <li class="task__item">
-            <button class="form__buttonDone form__buttonDone--click js-done">
-            ${task.done ? "✔" : ""}
-            </button>
-            <span class="form__text${ task.done ? "task__decoration" : ""}">
-            ${task.content} 
-            </span>
-            <button class="js-remove form__buttonRemove">
-            🗑
-            </button>
-         </li>
-                
-        `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-        bindEvents();
-    
     };
 
 
